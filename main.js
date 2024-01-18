6bd7299c0a17b530d06ded719d50b2a72799cf99
azfang <47017488+azfang@users.noreply.github.com>
2024-01-18 18:31:34 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+    // $("#syllabus_section2").hide();
+  });
+
+  $("a.smoothLink").click(function(e) {
+    // $(".right-column").addClass("fadeOut"); // anything with the "fadeOut" class will become transparent in 1s in our CSS
+
+    setTimeout( function(){ // wait 1s, then change URL
+        window.location = e.currentTarget.attributes['data-url'].value;
+    }, 1000);
+  });
+
+  // $(".right-column").fadeIn()
+
+
+// $("#click2").click(section2_Highlight);
+//   function section2_Highlight() {
+//     section1_FadeOut.play();
+//     section2_FadeIn.play();
+//     section3_FadeOut.play();
+//   }
+
+// $("#click3").click(section3_Highlight);
+//   function section3_Highlight() {
+//     section1_FadeOut.play();
+//     section2_FadeOut.play();
+//     section3_FadeIn.play();
+//   }
+
+// var section1_FadeOut = anime({
+//     targets: '#syllabus_section1',
+//     opacity: [
+//       { value: 0.05, duration: 800 },
+//     ],
+//     delay: 0,
+//     easing: 'easeInOutQuad',
+//     autoplay: false
+//   });
+
+// var section2_FadeOut = anime({
+//     targets: '#syllabus_section2',
+//     opacity: [
+//       { value: 0.05, duration: 800 },
+//     ],
+//     delay: 0,
+//     easing: 'easeInOutQuad',
+//     autoplay: false
+//   });
+
+// var section3_FadeOut = anime({
+//     targets: '#syllabus_section3',
+//     opacity: [
+//       { value: 0.05, duration: 800 },
+//     ],
+//     delay: 0,
+//     easing: 'easeInOutQuad',
+//     autoplay: false
+//   });
+
+// var section2_FadeIn = anime({
+//     targets: '#syllabus_section2',
+//     opacity: [
+//       { value: 1, duration: 800 },
+//     ],
+//     delay: 0,
+//     easing: 'easeInOutQuad',
+//     autoplay: false
+//   });
+
+// var section3_FadeIn = anime({
+//     targets: '#syllabus_section3',
+//     opacity: [
+//       { value: 1, duration: 800 },
+//     ],
+//     delay: 0,
+//     easing: 'easeInOutQuad',
+//     autoplay: false
+//   });
