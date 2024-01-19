9029d4e27ffd965ed095df362255af2edc53a22e
azfang <47017488+azfang@users.noreply.github.com>
2024-01-19 17:16:05 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,13 +6,13 @@
 //     });
 // });
 
-  $("a.smoothLink").click(function(e) {
-    // $(".right-column").addClass("fadeOut"); // anything with the "fadeOut" class will become transparent in 1s in our CSS
+  // $("a.smoothLink").click(function(e) {
+  //   // $(".right-column").addClass("fadeOut"); // anything with the "fadeOut" class will become transparent in 1s in our CSS
 
-    setTimeout( function(){ // wait 1s, then change URL
-        window.location = e.currentTarget.attributes['data-url'].value;
-    }, 1000);
-  });
+  //   setTimeout( function(){ // wait 1s, then change URL
+  //       window.location = e.currentTarget.attributes['data-url'].value;
+  //   }, 1000);
+  // });
 
   // $(".right-column").fadeIn()
 
